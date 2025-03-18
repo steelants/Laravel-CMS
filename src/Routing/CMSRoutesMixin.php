ae30a43b4e3d4b09e51d2cb744a7367d780d1d4d
JonatanRek <gamerclassn7@gmail.com>
2025-03-18 07:20:19 +0100

    Update CMSRoutesMixin.php

--- src/Routing/CMSRoutesMixin.php
+++ src/Routing/CMSRoutesMixin.php
@@ -9,9 +9,9 @@ class CMSRoutesMixin
         return function ($options = []) {
             $namespace = 'SteelAnts\LaravelCMS\Http\Controllers';
             $this->group(['namespace' => $namespace], function () use ($options) {
-                $this->get('/page/{slug}', 'PageController@show')->middleware(['web', 'auth'])->name('cms.page.show');
-                $this->get('/posts', 'PageController@index')->middleware(['web', 'auth'])->name('cms.page.index');
-                $this->get('/post/{post_id}', 'PageController@show')->middleware(['web', 'auth'])->name('cms.page.show');
+                $this->get('/page/{slug}', 'PageController@show')->middleware(['web'])->name('cms.page.show');
+                $this->get('/posts', 'PostController@index')->middleware(['web'])->name('cms.page.index');
+                $this->get('/post/{post_id}', 'PostController@show')->middleware(['web'])->name('cms.page.show');
             });
         };
     }
